6bc8b0ac6880bb0a450c225ea70319f456f73141
cfop <cfop0608@qq.com>
2018-11-17 15:31:13 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -40,4 +40,60 @@ $(function () {
             }
         })
     }
+
+    //2 点击添加按钮。，显示添加模态框
+   $("#addBtn").click(function () {
+        //    显示添加模态框
+        $("#addModal").modal("show");
+   })
+
+
+    //    3. 表单校验功能
+    $("#form").bootstrapValidator({
+        // 配置校验的图标 
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+          },
+        fields : {
+            categoryName : {
+                validators : {
+                    // 非空
+                    notEmpty : {
+                        message : "请输入一级分类"
+                    }
+                }
+            }
+        }
+    })
+
+
+    // 4.注册表单校验成功事件，阻止默认的提交，通过ajax提交
+    $("#form").on("success.form.bv",function (e) {
+        e.preventDefault();
+
+        // 通过ajax提交
+        $.ajax ({
+            url : "/category/addTopCategory",
+            type : "post",
+            data : $("#form").serialize(),
+            datatype : "json",
+            success : function (info) {
+                // console.log(info);
+                if (info.success) {
+                    // 添加成功
+                    // 关闭模态框
+                    $("#addModal").modal("hide");
+                    // 重新渲染第一页
+                    currentPage = 1;
+                    render();
+
+                    // 重置表单的内容和状态
+                $("#form").data("bootstrapValidator").resetForm(true);
+                }
+            }
+        })
+    })
+
 })
